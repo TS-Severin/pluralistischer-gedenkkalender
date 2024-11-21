8ea5a40ed505a12ae7f6e9c42065fbb96c20fc61
Tillmann Severin <tillmann.severin@posteo.de>
2024-11-21 17:35:05 +0100

    change Link to a in Downloadbutton

--- src/components/DateDownloadButton/DateDownloadButton.js
+++ src/components/DateDownloadButton/DateDownloadButton.js
@@ -1,15 +1,15 @@
-import Link from "next/link";
+
 
 export default function DateDownloadButton({ event }) {
-    const icalDownloadLink = `https://pluralistischer-gedenkkalender.vercel.app/api/ics-import/${event.id}`;
+    const icalDownloadLink = `/api/ics-import/${event.id}`;
 
     return (
 
-        <Link href={icalDownloadLink} download="event.ics">
+        <a href={icalDownloadLink} download="event.ics">
 
             <button>Download Event</button>
 
-        </Link>
+        </a>
 
     );
 };
