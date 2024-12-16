29f8a834b100b64b824da34279e5f7832af8e6f4
Tillmann Severin <tillmann.severin@posteo.de>
2024-12-16 13:16:33 +0100

    set initial opacity of dots in gsap animation to 1 in order to prevent 0 opacity bug

--- src/components/TimelineDot/useDotAnimation.js
+++ src/components/TimelineDot/useDotAnimation.js
@@ -8,7 +8,7 @@ export default function useEaseInAnimation(elementRef, index) {
     if (element) {
 
       // First, set all elements to be invisible initially
-      gsap.set(element, { opacity: 0, zIndex: 1000, ease: 'power3.out', delay: 0.5 + Math.random() * 2, });
+      gsap.set(element, { opacity: 1, zIndex: 1000, ease: 'power3.out', delay: 0.5 + Math.random() * 2, });
 
       // Then, animate the elements to fade in with a delay
 
